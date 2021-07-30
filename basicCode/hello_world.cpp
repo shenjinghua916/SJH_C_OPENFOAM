684ab40c9ca4894d8ec65da5b8843687f4b37403
shenjinghua916 <shenjinghua@mail.dlut.edu.cn>
2021-07-30 17:01:53 +0800

    hello world added

---
+++ basicCode/hello_world.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+	cout << "Hello World!";
+	return 0;
+}
+
+//credit:https://www.codingunit.com/cplusplus-tutorial-first-cplusplus-program-hello-world
